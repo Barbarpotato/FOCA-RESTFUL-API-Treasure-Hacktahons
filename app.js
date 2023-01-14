3131e3dd0552647a34d3aad6b6d4b908a52764d7
Darmawan <darmawanjr88@gmail.com>
2023-01-14 16:40:40 +0800

    test

--- app.js
+++ app.js
@@ -23,15 +23,9 @@ app.use((req, res, next) => {
   next();
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/sellers', sellersRouter);
 app.use('/contributor', contributorRouter)
