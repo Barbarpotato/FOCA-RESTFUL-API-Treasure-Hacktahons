ac5290c45dde8609b29f4eae895e2ae628ba5484
Darmawan <darmawanjr88@gmail.com>
2023-01-14 17:00:42 +0800

    proto2

--- utils/db.config.js
+++ utils/db.config.js
@@ -5,7 +5,9 @@ const firebase = require('firebase-admin');
 firebase.initializeApp({
     credential: firebase.credential.cert({
         project_id: process.env.PROJECT_ID,
-        private_key: process.env.PRIVATE_KEY,
+        private_key: process.env.PRIVATE_KEY
+            ? process.env.PRIVATE_KEY.replace(/\\n/gm, "\n")
+            : undefined,
         client_email: process.env.CLIENT_EMAIL,
     })
 })
